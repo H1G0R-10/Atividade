57bc714360a6db9360607a7d5e1fa207534d14a8
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-09-23 17:05:01 -0300

    Ativ

---
+++ somar/script.js
@@ -0,0 +1,8 @@
+function Somar(){
+
+        var n1 = parseFloat(document.getElementById("s1").value);
+        var n2 = parseFloat(document.getElementById("s2").value);
+        var n3 = parseFloat(document.getElementById("s3").value);
+        var resp = document.getElementById("respId");
+        resp.textContent = n1 + n2 + n3;
+}
